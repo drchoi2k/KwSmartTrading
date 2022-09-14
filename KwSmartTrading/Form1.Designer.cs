274a64af4aea54ee70ab9b209be4fce07ba56693
drchoi2k <drchoi2k@naver.com>
2022-09-14 13:25:30 +0900

    ㅁㅁㅁ

--- KwSmartTrading/Form1.Designer.cs
+++ KwSmartTrading/Form1.Designer.cs
@@ -9,7 +9,7 @@ namespace KwSmartTrading
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 사용 중인 모든 리소스를 정리합니다. 열심히 하고 있는 중입니다
+        /// 사용 중인 모든 리소스를 정리합니다. AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
         /// </summary>
         /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
         protected override void Dispose(bool disposing)
